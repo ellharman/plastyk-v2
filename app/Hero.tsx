ccdae74d7d340a8eb02cff6a7110dd731876a39b
Ell <78617707+ellharman@users.noreply.github.com>
2026-09-13 13:38:42 +0100

    feat: hero and alt image

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -1,26 +1,25 @@
 "use client";
 
-import Image from 'next/image'
+import Image from "next/image";
+import type { DetailedHTMLProps, VideoHTMLAttributes } from "react";
+
+const fillScreenStyle = {
+  height: "100vh",
+  width: "100vw",
+  objectFit: "cover",
+  objectPosition: "center",
+} as DetailedHTMLProps<VideoHTMLAttributes<HTMLVideoElement>, HTMLVideoElement>
 
 export default function Hero() {
   return (
     <div className="relative w-screen h-screen overflow-hidden">
-      <video
-        autoPlay
-        muted
-        playsInline
-        loop
-        style={{
-          height: "100vh",
-          width: "100vw",
-          objectFit: "cover",
-          objectPosition: "center",
-        }}
-      >
+      <video autoPlay muted playsInline loop style={fillScreenStyle}>
         <source src="/video/hero.mp4" type="video/mp4" />
       </video>
+      <Image id="reduced-motion-hero" src="/img/reduced-motion-hero.jpg" alt="" quality={100} fill unoptimized preload/>
+
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 text-center text-white">
-       <Image src="/img/logo-white-red.png" alt=""  />
+        <Image src="/img/logo-white-red.png" alt="" style={{filter: "drop-shadow(1px 1px 1px #000000)"}} />
       </div>
     </div>
   );
