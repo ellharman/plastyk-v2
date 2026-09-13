f408d423e9822e23e3dcfac3af0399437dbee0e3
Ell <78617707+ellharman@users.noreply.github.com>
2026-09-13 13:09:16 +0100

    feat: full hero with logo

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -1,20 +1,27 @@
 "use client";
 
+import Image from 'next/image'
+
 export default function Hero() {
   return (
-    <video
-      autoPlay
-      muted
-      playsInline
-      loop
-      style={{
-        height: "100vh",
-        width: "100vw",
-        objectFit: "cover",
-        objectPosition: "center",
-      }}
-    >
-      <source src="/app/assets/video/hero.mp4" type="video/mp4" />
-    </video>
+    <div className="relative w-screen h-screen overflow-hidden">
+      <video
+        autoPlay
+        muted
+        playsInline
+        loop
+        style={{
+          height: "100vh",
+          width: "100vw",
+          objectFit: "cover",
+          objectPosition: "center",
+        }}
+      >
+        <source src="/video/hero.mp4" type="video/mp4" />
+      </video>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 text-center text-white">
+       <Image src="/img/logo-white-red.png" alt=""  />
+      </div>
+    </div>
   );
 }
