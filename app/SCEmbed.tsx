43203ee9fefd09b534faba9649ec42745814d362
Ell <78617707+ellharman@users.noreply.github.com>
2026-09-16 14:39:25 +0100

    fix: soundcloud embed

--- app/SCEmbed.tsx
+++ app/SCEmbed.tsx
@@ -8,9 +8,9 @@ export default function SCEmbed() {
         height="450"
         scrolling="no"
         frameBorder="no"
-        allow="autoplay; encrypted-media"
+        allow="encrypted-media"
         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/soundcloud%253Aplaylists%253A2298107244&color=%23ff7bac&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
-        className="rounded-md"
+        className="rounded-sm"
       ></iframe>
       <div
         className="overflow-hidden whitespace-nowrap text-ellipsis break-normal text-[10px] font-thin text-muted-foreground"
